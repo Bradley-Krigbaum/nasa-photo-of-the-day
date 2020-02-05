be32c936990357f51067d1e8ba26f77bf7549c5f
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-02-04 22:14:02 -0700

    finished adding components and hooks

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <div className="#">
+            <h1>NASA'S ASTRONOMY PHOTO OF THE DAY</h1>
+        </div>
+    );
+    
+};
+
+
+
+export default NavBar;
